d79f1bea79aaf0bd27696cbeb6438aac6334efcc
Charles Truscott Watters <87652332+ctwtruscottwatters@users.noreply.github.com>
2022-06-27 08:48:15 -0700

    Add files via upload

--- primes_CharlesTruscott.py
+++ primes_CharlesTruscott.py
@@ -7,29 +7,15 @@ Charles Thomas Wallace Truscott
 
 def return_primes():
     primes = []
-    primesCount = []
-    count = 0
-    low = 2
-    for q in range(0, 1000000, 1000):
-        high = q
-        print("q: {} ".format(q))
-        for n in range(high, low, -1):
-            for x in range(2, n, 1):
-                if n % x == 0:
-                    continue
-                elif (n % x != 0) and (n in primes) == False:
-                    count += 1
-                    primes.append(n)
-            if high > 1000:
-                low = high - 1000
-        primesCount.append((count, high))
-        print(primesCount)
-#wrong: print("There are {} number of primes in the first {} natural numbers".format(primesCount[0], primesCount[1]))
-        count = 0
-
-                
-    print(primesCount)
-
+    divisorFound = False
+    for n in range(0, 1000, 1):
+        for x in range(2, n, 1):
+            if n % x == 0:
+                divisorFound = True
+        if divisorFound == False:
+            primes.append(n)
+        divisorFound = False
+    print(primes)
 def main():
     return_primes()
     
@@ -37,34 +23,7 @@ if __name__ == "__main__": main()
 
 
 """
-Almost surely a doubly exponentially costly time scale to this algorithm
-Works better to simply have a True False value for the prime check instead of adding to a list and complex logical operators and operands
-"""
-"""
 runfile('C:/Users/17/Downloads/primes_CharlesTruscott.py', wdir='C:/Users/17/Downloads')
-q: 0 
-[(0, 0)]
-q: 1000 
-[(0, 0), (998, 1000)]
-q: 2000 
-[(0, 0), (998, 1000), (1000, 2000)]
-q: 3000 
-
-Are there 998 primes in the first 1000 numbers and 1000 primes between 1000 and 2000?
-
-Minutes later and microprocessor overheating with fans running ... 3.0Ghz Quad Core Alienware 17 R2 32GB RAM DDR3 
-
-runcell(0, 'C:/Users/17/Downloads/primes_CharlesTruscott.py')
-q: 0 
-[(0, 0)]
-q: 1000 
-[(0, 0), (998, 1000)]
-q: 2000 
-[(0, 0), (998, 1000), (1000, 2000)]
-q: 3000 
-[(0, 0), (998, 1000), (1000, 2000), (1000, 3000)]
-q: 4000 
-
-
+[0, 1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
 
 """
